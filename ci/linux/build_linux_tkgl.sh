8bb783fa198c4b80821c4a261f6b2df83ce77022
Nathan Dunfield <nathan@dunfield.info>
2024-01-31 19:36:43 -0600

    CI: I can haz manylinux?

--- ci/linux/build_linux_tkgl.sh
+++ ci/linux/build_linux_tkgl.sh
@@ -15,14 +15,14 @@ mkdir tcl9 tk9 tcl8 tk8
 echo downloading tcl/tk source ...
 wget --no-check-certificate -O tcl8.tar.gz $TCL8_URL
 wget --no-check-certificate -O tk8.tar.gz $TK8_URL
-wget --no-check-certificate -O tcl9.tar.gz $TCL9_URL
-wget --no-check-certificate -O tk9.tar.gz $TK9_URL
+#wget --no-check-certificate -O tcl9.tar.gz $TCL9_URL
+#wget --no-check-certificate -O tk9.tar.gz $TK9_URL
 
 echo extracting source ...
 tar xf tcl8.tar.gz --directory=tcl8 --strip-components=1
 tar xf tk8.tar.gz --directory=tk8 --strip-components=1
-tar xf tcl9.tar.gz --directory=tcl9 --strip-components=1
-tar xf tk9.tar.gz --directory=tk9 --strip-components=1
+#tar xf tcl9.tar.gz --directory=tcl9 --strip-components=1
+#tar xf tk9.tar.gz --directory=tk9 --strip-components=1
 echo building tcl and tk
 cd tcl8/unix
 ./configure
@@ -31,21 +31,21 @@ cd ../../tk8/unix
 ./configure --with-tcl=../../tcl8/unix
 make -j4
 cd ../..
-cd tcl9/unix
-./configure
-make -j4
-cd ../../tk9/unix
-./configure --with-tcl=../../tcl9/unix
-make -j4
-cd ../..
+#cd tcl9/unix
+#./configure
+#make -j4
+#cd ../../tk9/unix
+#./configure --with-tcl=../../tcl9/unix
+#make -j4
+#cd ../..
 cp ci/linux/linux_configure configure
 echo building for tcl/tk 8
 ./configure --with-tcl=tcl8/unix --with-tk=tk8/unix
 make
 mv libTkgl1.0.so dist/Tkgl1.0
 rm -rf build
-echo building for tcl/tk 9
-./configure --with-tcl=tcl9/unix --with-tk=tk9/unix
-make
-mv libtcl9Tkgl1.0.so dist/Tkgl1.0
+#echo building for tcl/tk 9
+#./configure --with-tcl=tcl9/unix --with-tk=tk9/unix
+#make
+#mv libtcl9Tkgl1.0.so dist/Tkgl1.0
 mv pkgIndex.tcl dist/Tkgl1.0
