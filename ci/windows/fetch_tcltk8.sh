a93938882a7131ea5d6f930ee843fb37e40d3d38
Nathan Dunfield <nathan@dunfield.info>
2025-06-01 09:54:19 -0500

    cleaning up??

--- ci/windows/fetch_tcltk8.sh
+++ ci/windows/fetch_tcltk8.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 set -e
-TCL8_VERSION=8.6.16
-TK8_VERSION=8.6.16
+TCL8_VERSION=8.6.15
+TK8_VERSION=8.6.15
 # Find wget; on the ARM runners it's in the second location.
-WGET="/msys64/usr/bin/wget -nv"
+WGET="/msys64/usr/bin/wget"
 if [ ! -e $WGET ]; then
-    WGET="/usr/bin/wget -nv"
+    WGET="/usr/bin/wget"
 fi
-if [ ! -e tcl$TCL8_VERSION-src.tar.gz ] ; then
-    $WGET https://prdownloads.sourceforge.net/tcl/tcl$TCL8_VERSION-src.tar.gz
+if [ ! -e tcl$TCL8_VERSION-src.tar.gz ]; then
+    $WGET -nv https://prdownloads.sourceforge.net/tcl/tcl$TCL8_VERSION-src.tar.gz
 fi
 if [ ! -e tk$TK8_VERSION-src.tar.gz ]; then
-    $WGET https://prdownloads.sourceforge.net/tcl/tk$TCL8_VERSION-src.tar.gz
+    $WGET -nv https://prdownloads.sourceforge.net/tcl/tk$TCL8_VERSION-src.tar.gz
 fi
 rm -rf tcl8 tk8
 tar xfz tcl$TCL8_VERSION-src.tar.gz
